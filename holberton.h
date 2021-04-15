a60705e1a157e1a2cb4a7e9d1e22505e3c825e92
Jaime garcia <62820685+BlackThunder24@users.noreply.github.com>
2021-04-14 23:35:21 -0500

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -1,7 +1,6 @@
 #ifndef holberton_h
 #define holberton_h
 
-/*Declaracion de librerias /
 #include <stdio.h>
 #include <stdio_ext.h>
 #include <stdlib.h>
@@ -12,17 +11,14 @@
 #define MAX_ARGS 13
 #define MAX_I 105
 
-/Declarando variables/
 char comando[MAX_I];
 char args[MAX_ARGS];
 
-/Declarando variables de ambiente/
 char SHELL[MAX_I];
 char PATH[MAX_I];
 char HOME[MAX_I];
 char PWD[MAX_I];
 
-/Declarando cabeceras de funciones/
 void separaArgs(void);
 void listaDir(void);
 void comExterno(void); 
