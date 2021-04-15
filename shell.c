a228ab7b5e990d3ae9eb12a4c046701420fcd5a5
Jaime garcia <62820685+BlackThunder24@users.noreply.github.com>
2021-04-14 23:01:43 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 
 main(void){
-int welcome=1;
+int welcome = 1;
 getcwd(PWD, MAX_I);
-strcpy(PATH,getenv("PATH"));
-strcpy(HOME,getenv("PATH"));
-strcpy(SHELL,PWD);
-do{
-printf("%s:p",PWD);
+strcpy(PATH, getenv("PATH"));
+strcpy(HOME, getenv("PATH"));
+strcpy(SHELL, PWD);
+do {
+printf("%s:p", PWD);
 __fpurge(stdin);
-menset(comand,'\0',MAX_I)
-scanf("%[^\n]s",comand);
-if(strlen(comand)>0){
+menset(comand, '\0', MAX_I)
+scanf("%[^\n]s", comand);
+if(strlen(comand)>0) {
 separaArgs();
-if(strcmp(comand,"cd")==0){
-if(args[1])
-if(chdir(args[1]!=0))
+if (strcmp(comand,"cd")==0) {
+if (args[1])
+if (chdir(args[1]!=0))
 printf("Error, %s doesn't exist",args[1])
 else getcwd(PWD, MAX_I);
 }
