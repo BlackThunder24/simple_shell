7f04849896738aebb58fb8141fbf868ffc1b9bce
Jaime garcia <62820685+BlackThunder24@users.noreply.github.com>
2021-04-15 22:24:57 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 
-int main(void){
+int main(void)
+{
 int continuar = 1;
 getcwd(PWD, MAX_I);
 strcpy(PATH, getenv("PATH"));
@@ -14,17 +15,20 @@ scanf("%[^\n]s", comando);
 if (strlen(comando) > 0)
 {
 separaArgs();
-if (strcmp(comando, "cd") == 0){
+if (strcmp(comando, "cd") == 0)
+{
 if (args[1])
 if (chdir(args[1]) != 0)
 printf("Error, %s doesn't exist", args[1]);
-else getcwd(PWD, MAX_I);
+else
+getcwd(PWD, MAX_I);
 }
 else if (strcmp(comando, "dir") == 0)
 listaDir();
 else if (strcmp(comando, "clr") == 0)
 strcpy(comando, "clear"), comExterno();
-else if (strcmp(comando, "env") == 0){
+else if (strcmp(comando, "env") == 0)
+{
 printf(" Variables de ambiente:\n");
 printf("  HOME=%s\n  PWD=%s\n", HOME, PWD);
 printf("  SHELL=%s\n  PATH=%s\n", SHELL, PATH);
@@ -50,7 +54,8 @@ if (pid < 0)
 printf("Error! no se pudo crear un proceso hijo");
 if (pid == 0){
 status = execvp(comando, args);
-if (status){
+if (status)
+{
 printf("Error! %s no se reconoce o no se pudo ejecutar\n", comando);
 exit(1);
 }
