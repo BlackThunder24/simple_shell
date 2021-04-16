1ac495d75026b531b9d2291fb9bf019e7f9d00ac
Jaime garcia <62820685+BlackThunder24@users.noreply.github.com>
2021-04-15 22:06:05 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -13,33 +13,34 @@ printf("%s>", PWD);
 __fpurge(stdin);
 memset(comando, '\0', MAX_I);
 scanf("%[^\n]s", comando);
-if(strlen(comando) > 0)
+if (strlen(comando) > 0)
 {
 separaArgs();
-if(strcmp(comando, "cd") == 0))
+if (strcmp(comando, "cd") == 0)
 {
-if(args[1])
-if(chdir(args[1]) != 0)
+if (args[1])
+if (chdir(args[1]) != 0)
 printf("Error, %s doesn't exist", args[1]);
-else getcwd(PWD, MAX_I);
+else
+getcwd(PWD, MAX_I);
 }
-else if(strcmp(comando, "dir") == 0)
+else if (strcmp(comando, "dir") == 0)
 listaDir(); 
-else if(strcmp(comando, "clr") == 0)
+else if (strcmp(comando, "clr") == 0)
 strcpy(comando, "clear"), comExterno();
 
-else if(strcmp(comando, "env") == 0){
+else if (strcmp(comando, "env") == 0){
 printf(" Variables de ambiente:\n");
 printf("  HOME=%s\n  PWD=%s\n", HOME, PWD);
 printf("  SHELL=%s\n  PATH=%s\n", SHELL, PATH);
 }
-else if(strcmp(comando, "echo")==0)
+else if (strcmp(comando, "echo")==0)
 {
-if(args[1]) eco();
+if (args[1]) eco();
 }
-else if(strcmp(comando, "pwd") == 0)
+else if (strcmp(comando, "pwd") == 0)
 printf("%s\n",PWD);
-else if(strcmp(comando, "exit") == 0)
+else if (strcmp(comando, "exit") == 0)
 continuar =0;
 else comExterno();
 }
